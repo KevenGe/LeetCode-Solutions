3dd5aef4710ef5ff36c82133e9b04468f94f0d4c
Keven Ge <1985996902@qq.com>
2021-07-28 00:17:05 +0800

    add: 84. 柱状图中最大的矩形

--- Solutions for Python/84. 柱状图中最大的矩形.py
+++ Solutions for Python/84. 柱状图中最大的矩形.py
@@ -1,5 +1,30 @@
-from typing import  List
+# 84. 柱状图中最大的矩形
+# https://leetcode-cn.com/problems/largest-rectangle-in-histogram/
+# 单调栈解法
+
+from typing import List
+
 
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        pass
+        heights = heights + [0]
+        ans = 0
+        sta = [[0, -1]]
+        for i, height in enumerate(heights):
+            if sta[-1][0] < height:
+                sta.append([height, i])
+            elif sta[-1][0] == height:
+                sta[-1][1] = i
+            else:
+                while sta[-1][0] > height:
+                    h, i2 = sta.pop()
+                    ans = max(ans, (i - sta[-1][1] - 1) * h)
+                sta.append([height, i])
+
+        return ans
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.largestRectangleArea([0]))
+
