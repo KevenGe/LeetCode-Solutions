e4563fc76752046c650e37ac8c6a7ceb78641a84
Keven Ge <1985996902@qq.com>
2021-07-27 11:33:03 +0800

    update: 剑指 Offer 52. 两个链表的第一个公共节点

--- 《剑指offer》/剑指 Offer 52. 两个链表的第一个公共节点.py
+++ 《剑指offer》/剑指 Offer 52. 两个链表的第一个公共节点.py
@@ -1,9 +1,6 @@
 # 剑指 Offer 52. 两个链表的第一个公共节点
 # https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
 
-# TODO: 此题未做出来。题解中，不明白为什么存在None的情况，待解决。
-
-
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
