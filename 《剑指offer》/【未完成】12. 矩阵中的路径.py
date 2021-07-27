eb00316df0af052683a2d44e080e730ad976a1e5
Keven Ge <1985996902@qq.com>
2021-07-27 11:18:19 +0800

    update: 剑指 Offer 12. 矩阵中的路径

--- 《剑指offer》/【未完成】12. 矩阵中的路径.py
+++ 《剑指offer》/【未完成】12. 矩阵中的路径.py
@@ -1,3 +1,6 @@
+# 剑指 Offer 12. 矩阵中的路径
+# https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof/
+
 from typing import List
 
 
@@ -6,38 +9,42 @@ class Solution:
         if len(board) == 0 or word == 0:
             return False
 
-        def search(i, j):
-            dp = [[False for i_ in range(len(board[0]))] for j_ in range(len(board))]
-            wordHasLen = 0
-            isFound = False
+        def search(i, j, word: str):
 
-            def dfs(i, j):
-                """"""
-                global wordHasLen, isFound
-                if isFound is True:
-                    return
-                if wordHasLen == len(word):
-                    isFound = True
-                    return
+            dp = [[False for j in range(len(board[0]))] for i in range(len(board))]
 
-                if i >= 0 and i < len(board) and j >= 0 and j < len(board[0]):
-                    if dp[i][j] is False and dp[i][j] == word[wordHasLen]:
-                        dp[i][j] = True
-                        wordHasLen += 1
-                        dfs(i + 1, j)
-                        dfs(i - 1, j)
-                        dfs(i, j + 1)
-                        dfs(i, j - 1)
-                        wordHasLen -= 1
-                        dp[i][j] = False
+            def f(i, j, word: str, dp):
+                if len(word) == 0:
+                    return True
 
-            dfs(i, j)
-            return isFound
+                if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
+                    return False
+
+                if dp[i][j] == False and board[i][j] == word[0]:
+                    dp[i][j] = True
+                    ans = (
+                        f(i + 1, j, word[1:], dp)
+                        or f(i - 1, j, word[1:], dp)
+                        or f(i, j - 1, word[1:], dp)
+                        or f(i, j + 1, word[1:], dp)
+                    )
+                    dp[i][j] = False
+                    return ans
+                else:
+                    return False
+
+            return f(i, j, word, dp)
 
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == word[0]:
-                    if search(i, j):
+                    if search(i, j, word):
                         return True
 
         return False
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.exist([["a", "a"]], "aaa"))
+
