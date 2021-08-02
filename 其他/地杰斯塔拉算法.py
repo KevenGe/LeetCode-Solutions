4392044706260e7890c46b10185c9811e61888e0
Keven Ge <1985996902@qq.com>
2021-08-02 14:55:25 +0800

    add: 743. 网络延迟时间

---
+++ 其他/地杰斯塔拉算法.py
@@ -0,0 +1,43 @@
+# 743. 网络延迟时间
+# https://leetcode-cn.com/problems/network-delay-time/
+
+from typing import List
+from queue import PriorityQueue
+
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # getD
+        d = {}
+        for i in range(1,n+1):
+            d[i] = []
+        for ti in times:
+            d[ti[0]].append([ti[1], ti[2]])
+
+        dis = [100000000] * (n + 1)
+        dis[k] = 0
+
+        q = PriorityQueue()
+        q.put([0, k])
+        while q.empty() != True:
+            t = q.get()
+
+            if t[0] > dis[t[1]]:
+                continue
+
+            for tar, di in d[t[1]]:
+                if dis[tar] > dis[t[1]] + di:
+                    dis[tar] = dis[t[1]] + di
+                    q.put([dis[tar], tar])
+
+        mm = 0
+        for i in range(1, len(dis)):
+            mm = max(mm, dis[i])
+
+        return mm if mm != 100000000 else -1
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.networkDelayTime([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2))
+
