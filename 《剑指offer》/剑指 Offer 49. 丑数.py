52e806fd6d034d5c69ddd274c08e563e93630b3f
Keven Ge <1985996902@qq.com>
2021-08-04 14:52:56 +0800

    add: 剑指 Offer 49. 丑数

---
+++ 《剑指offer》/剑指 Offer 49. 丑数.py
@@ -0,0 +1,24 @@
+# 剑指 Offer 49. 丑数
+# https://leetcode-cn.com/problems/chou-shu-lcof/
+
+
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        dp = [1] * n
+        p2 = 0
+        p3 = 0
+        p5 = 0
+        for i in range(1, n):
+            t = min(dp[p2] * 2, dp[p3] * 3, dp[p5] * 5)
+            if t == dp[p2] * 2:
+                p2 += 1
+            if t == dp[p3] * 3:
+                p3 += 1
+            if t == dp[p5] * 5:
+                p5 += 1
+            dp[i] = t
+        return dp[n-1]
+
+
+if __name__ == "__main__":
+    solution = Solution()
