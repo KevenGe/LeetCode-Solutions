f31a3c83003f39e4f88ff28f125f0319871ca720
Keven Ge <1985996902@qq.com>
2021-09-03 14:32:37 +0800

    add: 面试题 17.14. 最小K个数

--- 程序员面试金典（第 6 版）/面试题 17.14. 最小K个数.py
+++ 程序员面试金典（第 6 版）/面试题 17.14. 最小K个数.py
@@ -2,16 +2,42 @@
 # https://leetcode-cn.com/problems/smallest-k-lcci/
 
 from typing import List
-import heapq
 
 
 class Solution:
     def smallestK(self, arr: List[int], k: int) -> List[int]:
-        heapq.heapify(arr)
         ans = []
+        self.fastSort(arr, k, 0, len(arr) - 1)
+
         for i in range(k):
-            ans.append(heapq.heappop(arr))
+            ans.append(arr[i])
+
         return ans
+
+    def fastSort(self, arr: List[int], k, left, right):
+        if k == 0 or right - left <= 0:
+            return
+        elif right - left == 1:
+            if arr[left] > arr[right]:
+                arr[left], arr[right] = arr[right], arr[left]
+        else:
+            l = left
+            r = right
+            m = right
+
+            while l < r:
+                while l < r and arr[l] <= arr[m]:
+                    l += 1
+                while l < r and arr[r] >= arr[m]:
+                    r -= 1
+
+                if l != r:
+                    arr[l], arr[r] = arr[r], arr[l]
+
+            arr[l], arr[m] = arr[m], arr[l]
+            self.fastSort(arr, k, left, l - 1)
+            if l <= k - 2:
+                self.fastSort(arr, k, l + 1, right)
 
 
 if __name__ == "__main__":
