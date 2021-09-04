e3617e1048375e86d688d28ece97499dae08885b
Keven Ge <1985996902@qq.com>
2021-09-04 10:34:01 +0800

    add: 【待完成】LCP 13. 寻宝

---
+++ 力扣杯/【待完成】LCP 13. 寻宝.py
@@ -0,0 +1,13 @@
+# LCP 13. 寻宝
+# https://leetcode-cn.com/problems/xun-bao/
+
+from typing import List
+
+
+class Solution:
+    def minimalSteps(self, maze: List[str]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
