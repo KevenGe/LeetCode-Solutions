a9c3fcecde2f5ef2042dae0d2eee5c936b693e42
Keven Ge <1985996902@qq.com>
2021-09-10 10:54:31 +0800

    add: 1894. 找到需要补充粉笔的学生编号

---
+++ Solutions for Python/1894. 找到需要补充粉笔的学生编号.py
@@ -0,0 +1,23 @@
+# 1894. 找到需要补充粉笔的学生编号
+# https://leetcode-cn.com/problems/find-the-student-that-will-replace-the-chalk/
+
+################################################################################
+
+from typing import List
+
+
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        sums = sum(chalk)
+        k = k % sums
+
+        for i in range(len(chalk)):
+            k = k - chalk[i]
+            if k < 0:
+                return i
+
+
+################################################################################
+
+if __name__ == "__main__":
+    solution = Solution()
