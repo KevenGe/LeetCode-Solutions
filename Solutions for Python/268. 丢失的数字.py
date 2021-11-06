3064a187913f2ac9474a15d91b5aea04b903b8fb
Keven Ge <1985996902@qq.com>
2021-11-06 13:24:17 +0800

    add: 268. 丢失的数字

--- Solutions for Python/268. 丢失的数字.py
+++ Solutions for Python/268. 丢失的数字.py
@@ -2,14 +2,26 @@
 # https://leetcode-cn.com/problems/missing-number/
 
 ################################################################################
+# from typing import List
+# from functools import reduce
+
+
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         t = reduce(lambda x, y: x ^ y, list(range(len(nums) + 1)), 0)
+#         ans = reduce(lambda x, y: x ^ y, nums, t)
+#         return ans
+
+
+################################################################################
 from typing import List
 from functools import reduce
 
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        t = reduce(lambda x, y: x ^ y, list(range(len(nums) + 1)), 0)
-        ans = reduce(lambda x, y: x ^ y, nums, t)
+        t = reduce(lambda x, y: x + y, list(range(len(nums) + 1)), 0)
+        ans = reduce(lambda x, y: x - y, nums, t)
         return ans
 
 
