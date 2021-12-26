fdd290727bf70691f76ff9bda9b0e6eafb91f0b4
Keven Ge <1985996902@qq.com>
2021-12-26 16:52:42 +0800

    add: 1078. Bigram 分词

---
+++ Solutions for Python/1078. Bigram 分词.py
@@ -0,0 +1,12 @@
+# 1078. Bigram 分词
+# https://leetcode-cn.com/problems/occurrences-after-bigram/
+
+
+class Solution:
+    def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
+        ans = []
+        text = text.split(" ")
+        for i in range(len(text)-2):
+            if text[i] == first and text[i+1] == second:
+                ans.append(text[i+2])
+        return ans
