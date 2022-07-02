6ffe69d5858b09db538364bf3480d89626913e6e
Keven Ge <1985996902@qq.com>
2022-07-02 21:59:04 +0800

    update: 871. 最低加油次数

--- Solutions for Python/871. 最低加油次数.py
+++ Solutions for Python/871. 最低加油次数.py
@@ -3,6 +3,7 @@
 
 
 from typing import List
+from queue import PriorityQueue
 
 
 class Solution:
@@ -24,6 +25,51 @@ class Solution:
         return ans
 
 
-class TestSolution:
-    def test_1(self):
-        pass
+from queue import PriorityQueue
+
+
+class Solution:
+    def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
+        pri = PriorityQueue()
+
+        cur_dist = startFuel
+
+        count = 0
+
+        for i in range(len(stations)):
+            if cur_dist >= stations[i][0]:
+                pri.put_nowait(-stations[i][1])
+            else:
+
+                while True:
+                    if pri.qsize() == 0:
+                        return -1
+
+                    cur_dist = cur_dist + (-pri.get_nowait())
+                    count += 1
+
+                    if cur_dist >= stations[i][0]:
+                        break
+
+                pri.put_nowait(-stations[i][1])
+
+        if cur_dist >= target:
+            return count
+        else:
+
+            while True:
+                if pri.qsize() == 0:
+                    return -1
+
+                cur_dist = cur_dist + (-pri.get_nowait())
+                count += 1
+
+                if cur_dist >= target:
+                    break
+
+            return count
+
+
+if __name__ == "__main__":
+    so = Solution()
+    assert so.minRefuelStops(100, 25, [[25, 25], [50, 25], [75, 25]]) == 3
