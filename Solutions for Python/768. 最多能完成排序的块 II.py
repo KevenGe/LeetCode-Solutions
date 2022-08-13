96f3e6717e32ff446fca60a745802be13b05df18
Keven Ge <1985996902@qq.com>
2022-08-13 17:44:39 +0800

    update: 768. 最多能完成排序的块 II

--- Solutions for Python/768. 最多能完成排序的块 II.py
+++ Solutions for Python/768. 最多能完成排序的块 II.py
@@ -4,36 +4,63 @@
 from typing import List, Dict
 
 
+# class Solution:
+#     def maxChunksToSorted(self, arr: List[int]) -> int:
+#         """普通遍历解法"""
+#         arrs = sorted(arr)
+#         nexti: Dict[int, int] = dict()
+#         for i, a in enumerate(arrs):
+#             if a not in nexti:
+#                 nexti[a] = i
+
+#         ri = 0
+#         m = -1
+#         mi = 0
+#         count = 0
+
+#         for ai, a in enumerate(arr):
+#             if nexti[a] > ri:
+#                 ri = nexti[a]
+
+#             if a == m:
+#                 mi += 1
+#                 ri += 1
+#             elif a > m:
+#                 m = a
+#                 mi = 1
+
+#             if ai == ri:
+#                 count += 1
+#                 ri = ai + 1
+#                 m = -1
+#                 mi = 0
+
+#         return count
+
+
+
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
+        """单调栈解法"""
+        st = []
 
-        arrs = sorted(arr)
-        nexti: Dict[int, int] = dict()
-        for i, a in enumerate(arrs):
-            if a not in nexti:
-                nexti[a] = i
-
-        ri = 0
-        m = -1
-        mi = 0
         count = 0
 
-        for ai, a in enumerate(arr):
-            if nexti[a] > ri:
-                ri = nexti[a]
+        for a, b in zip(arr, sorted(arr)):
+            if len(st) == 0:
+                st.append(a)
+            else:
+                if st[-1] > a:
+                    pass
+                elif st[-1] == a:
+                    st.append(a)
+                elif st[-1] < a:
+                    st = [a]
 
-            if a == m:
-                mi += 1
-                ri += 1
-            elif a > m:
-                m = a
-                mi = 1
-
-            if ai == ri:
-                count += 1
-                ri = ai + 1
-                m = -1
-                mi = 0
+            if st[-1] == b:
+                st.pop()
+                if len(st) == 0:
+                    count += 1
 
         return count
 
