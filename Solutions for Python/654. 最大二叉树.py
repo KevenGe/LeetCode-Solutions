43614a1c221d63a83b77313a8093a7ef94a17cc3
Keven Ge <1985996902@qq.com>
2022-08-20 14:41:10 +0800

    add: 654. 最大二叉树

--- Solutions for Python/654. 最大二叉树.py
+++ Solutions for Python/654. 最大二叉树.py
@@ -2,7 +2,7 @@
 # https://leetcode.cn/problems/maximum-binary-tree/
 
 
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -12,17 +12,47 @@ class TreeNode:
         self.right = right
 
 
+# class Solution:
+#     def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+#         if len(nums) == 0:
+#             return None
+
+#         m = nums.index(max(nums))
+#         return TreeNode(
+#             val=nums[m],
+#             left=self.constructMaximumBinaryTree(nums[:m]),
+#             right=self.constructMaximumBinaryTree(nums[m + 1 :]),
+#         )
+
+
 class Solution:
     def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
-        if len(nums) == 0:
-            return None
 
-        m = nums.index(max(nums))
-        return TreeNode(
-            val=nums[m],
-            left=self.constructMaximumBinaryTree(nums[:m]),
-            right=self.constructMaximumBinaryTree(nums[m + 1 :]),
-        )
+        s = []
+        t: Dict[int, TreeNode] = dict()
+
+        for num in nums:
+            t[num] = TreeNode(val=num)
+        
+            l = -1
+            while len(s) > 0 and num > s[-1]:
+                p = s.pop()
+                if l != -1:
+                    t[p].right = t[l]
+                l = p
+
+            if l != -1:
+                t[num].left = t[l]
+            s.append(num)
+
+        l = -1
+        while len(s) > 0:
+            p = s.pop()
+            if l != -1:
+                t[p].right = t[l]
+            l = p
+
+        return t[l]
 
 
 if __name__ == "__main__":
