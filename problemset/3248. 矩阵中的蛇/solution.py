1d0f89499d97ffa5a63c0fa96c7b74b86084dec6
KevenGe <1985996902@qq.com>
2024-11-21 21:27:10 +0800

    add: 3248. 矩阵中的蛇

---
+++ problemset/3248. 矩阵中的蛇/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
+        ans = 0
+        for c in commands:
+            if c[0] == "U":
+                ans -= n
+            elif c[0] == 'D':
+                ans += n
+            elif c[0] == 'L':
+                ans -= 1
+            else:
+                ans += 1
+        return ans
